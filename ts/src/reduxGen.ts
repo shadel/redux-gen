7ce529670ad8625e79c4cb43a4142556df27a6fc
Trieu Vo <trieu.vo@insightus.com.au>
2018-07-16 17:05:42 +0700

    remove useless code

--- ts/src/reduxGen.ts
+++ ts/src/reduxGen.ts
@@ -1,24 +1,5 @@
 import {Reducer, Action, Dispatch, combineReducers} from 'redux';
 
-// class ActionId {
-//     constructor(private _value: string){};
-//     get value() {
-//         return this._value;
-//     }
-// }
-
-// const genUniqueActionId = <T extends any, K extends keyof T>(idValue: string) => {
-//     class UniqueId<K> extends ActionId {
-//         private id: UniqueId<K>;
-//         constructor(value: string) {
-//             super(value);
-//             this.id = new UniqueId<K>(value);
-//         };
-//     }
-//     const uniqueID = new UniqueId<K>(idValue);
-//     type uniqueID = typeof uniqueID;
-//     return uniqueID;
-// }
 export type AType = any;
 
 export type UniqueType<T extends any, K extends keyof T, TypeMethod> = T & K & TypeMethod;
@@ -32,10 +13,6 @@ export interface IATypeExtend<T extends AType, TypeMethod> extends AType {
     type: T,
     method: TypeMethod
 }
-// const extendAType = <T extends AType>(type: T, method: TypeMethod): IATypeExtend<T, typeof method> => ({
-//     type: `${type}\\${method}` as T,
-//     method
-// });
 
 export interface IRGAction<PT extends any, T extends AType = AType> extends Action<T> {
     payload: PT
@@ -77,19 +54,6 @@ export interface IReduxGenResult<T extends any> {
     actions: ActionMap<T>
 }
 
-// const createSingleReducer = <T extends any, A extends IRGAction<T[K], UniqueType<T, K, TypeMethod>>, K extends keyof T>(defaultValue: T[K], actionMap: IAction<T, K>) => (
-//   state: T[K] = defaultValue,
-//   action: A
-// ) => {
-//     switch (action.type) {
-//         case actionMap.create.action.type: 
-//             return action.payload;
-//         case actionMap.update.action.type: 
-//                 return action.payload;
-//     }
-//   return state;
-// }
-
 const getKeys = <T extends any>(o: T): Array<keyof T> => {
     const keys:Array<keyof T> = [];
     for (let key in o) {
@@ -97,30 +61,6 @@ const getKeys = <T extends any>(o: T): Array<keyof T> => {
     }
     return keys;
 }
-
-// const createCombineReducer = <T extends object, A extends IRGAction<T[K], UniqueType<T, K, TypeMethod>>, K extends keyof T>(defaultValue: T[K], actionMap: ActionMap<T[K]>) => (
-//     state: T[K] = defaultValue,
-//     action: A
-//   ) => {
-//     const reducers = getKeys(defaultValue).map((key) => {
-//         const singleValue = defaultValue[key];
-//         const red = reducerCreator(singleValue, actionMap[key]);
-//         return {
-//             [key]: red
-//         };
-//     }).reduce((res, red) => ({
-//         ...res,
-//         ...red
-//     }), {});
-//     return combineReducers(reducers);
-//   }
-// const reducerCreator = <T extends any, K extends keyof T>(reducerObject: T[K], actions: IAction<T, K>) => {
-//     if (!(typeof reducerObject === 'object') || reducerObject.length) {
-//         return createSingleReducer(reducerObject, actions);
-//     }
-//     const obj = reducerObject as object;
-//     return createCombineReducer(reducerObject, actions.map);
-// }
 
 const createSingleReducer = <
 T extends any, 
